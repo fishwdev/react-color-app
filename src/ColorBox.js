016127041ee7f78e4dd5c785a417e9c10363b5f1
fishwdev <fishwdev@gmail.com>
2022-02-23 14:55:30 +0800

    make 'copy' button in the ColorBox component workable by using the react-copy-to-cliboard

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,19 +1,22 @@
 import React, {Component} from "react";
+import {CopyToClipboard} from "react-copy-to-clipboard/src";
 import './ColorBox.css';
 
 class ColorBox extends Component {
     render() {
         const {background, name} = this.props;
         return(
-            <div className='ColorBox' style={{background: background}}>
-                <div className='ColorBox-copy-container copy-container'>
-                    <div className='ColorBox-box-content box-content'>
-                        <span>{name}</span>
+            <CopyToClipboard text={background}>
+                <div className='ColorBox' style={{background: background}}>
+                    <div className='ColorBox-copy-container copy-container'>
+                        <div className='ColorBox-box-content box-content'>
+                            <span>{name}</span>
+                        </div>
+                        <button className='ColorBox-copy-button copy-button'>Copy</button>
                     </div>
-                    <button className='ColorBox-copy-button copy-button'>Copy</button>
+                    <span className='ColorBox-view-more'>More</span>
                 </div>
-                <span className='ColorBox-view-more'>More</span>
-            </div>
+            </CopyToClipboard>
         )
     };
 }
