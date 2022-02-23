c4363c9e700ee28a84f2b32b63252a86b1f6c7c2
fishwdev <fishwdev@gmail.com>
2022-02-23 14:22:43 +0800

    style the 'copy' and 'more' button and the name of the ColorBox component

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,10 +3,16 @@ import './ColorBox.css';
 
 class ColorBox extends Component {
     render() {
+        const {background, name} = this.props;
         return(
-            <div className='ColorBox' style={{background: this.props.background}}>
-                <span>{this.props.name}</span>
-                <span>MORE</span>
+            <div className='ColorBox' style={{background: background}}>
+                <div className='ColorBox-copy-container copy-container'>
+                    <div className='ColorBox-box-content box-content'>
+                        <span>{name}</span>
+                    </div>
+                    <button className='ColorBox-copy-button copy-button'>Copy</button>
+                </div>
+                <span className='ColorBox-view-more'>More</span>
             </div>
         )
     };
