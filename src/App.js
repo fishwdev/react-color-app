bd5b7b8a99ab0e1e12aa8a6e12bfaf7656ba90d4
fishwdev <fishwdev@gmail.com>
2022-02-28 14:44:48 +0800

    introduced basic Router structure to the App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import './App.css';
+import {Route, Switch} from "react-router-dom";
 import Palette from "./Palette";
 import seedPalettes from "./seedPalettes";
 import {generatePalette} from "./colorHelper";
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Palette palette={generatePalette(seedPalettes[0])}/>
-    </div>
-  );
+    function findPalette(id) {
+        return seedPalettes.find((palette) => {
+            return palette.id === id;
+        })
+    }
+
+    return (
+        <div className="App">
+            <Switch>
+                <Route exact path='/' render={() => <h1>/ goes here</h1>}/>
+                <Route exact path='/palette/:id' render={(routeProps) =>
+                    <Palette palette={generatePalette(findPalette(routeProps.match.params.id))}/>}
+                />
+            </Switch>
+            <Palette palette={generatePalette(seedPalettes[0])}/>
+        </div>
+    );
 }
+
 
 export default App;
