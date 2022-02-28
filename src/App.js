97120c4df7ce97acdcdce95d99e509191f077b42
fishwdev <fishwdev@gmail.com>
2022-02-28 10:20:40 +0800

    updated props passed down from App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Palette from "./Palette";
 import seedPalettes from "./seedPalettes";
+import {generatePalette} from "./colorHelper";
 
 function App() {
   return (
     <div className="App">
-      <Palette {...seedPalettes[0]}/>
+      <Palette palette={generatePalette(seedPalettes[0])}/>
     </div>
   );
 }
