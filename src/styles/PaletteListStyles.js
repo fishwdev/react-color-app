9b4a3d6e0c1441a7c3ab626e0abd99347c1b7412
fishwdev <fishwdev@gmail.com>
2022-03-18 22:35:23 +0800

    added responsive styles to the PaletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from "./sizes";
+
 export default {
     root: {
         backgroundColor: 'blue',
@@ -7,11 +9,17 @@ export default {
         justifyContent: 'center'
     },
     container: {
-        width: '50%',
+        width: '60%',
         display: 'flex',
         alignItems: 'flex-start',
         flexDirection: 'column',
-        flexWrap: 'wrap'
+        flexWrap: 'wrap',
+        [sizes.down('xl')]: {
+            width: '70%'
+        },
+        [sizes.down('xs')]: {
+            width: '65%'
+        }
     },
     nav: {
         display: 'flex',
@@ -28,6 +36,14 @@ export default {
         width: '100%',
         display: 'grid',
         gridTemplateColumns: 'repeat(3, 30%)',
-        gridGap: '5%'
+        gridGap: '2.5rem',
+        [sizes.down('md')]: {
+            gridTemplateColumns: 'repeat(2, 50%)',
+            gridGap: '2rem'
+        },
+        [sizes.down('xs')]: {
+            gridTemplateColumns: 'repeat(1, 100%)',
+            gridGap: '1.5rem'
+        }
     }
 };
